9da95952a4b9fe9e95e951197bdb36c56f72df91
Soham Acharya <sohamacharya7@gmail.com>
2025-12-02 00:44:50 +0530

    code for just the randq function

---
+++ Code-Implementations/Randomised-Quicksort/RandQ.py
@@ -0,0 +1,18 @@
+import random
+import time
+import sys
+
+sys.setrecursionlimit(10000)
+
+
+def randomized_quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    pivot = random.choice(arr)
+
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+
+    return randomized_quicksort(left) + middle + randomized_quicksort(right)
